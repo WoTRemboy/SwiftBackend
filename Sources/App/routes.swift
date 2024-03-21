34d0008a21b4ac65cf0af6de7a628806c2fcb790
Roman Tverdokhleb <tverpokhdeb@icloud.com>
2024-03-21 21:37:26 +0300

    added some template requests

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -2,9 +2,43 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-app.get { req async in
-        "Testing the display of new changes"
+    app.get { req async in
+        "CandleHub server is online!"
     }
-
+    
+    app.get("hello", ":name") { req async throws -> String in
+        let name = try req.parameters.require("name")
+        return "Hello, \(name.capitalized)!"
+    }
+    
+    app.get("json", ":name") { req async throws -> UserResponse in
+        let name = try req.parameters.require("name")
+        let message = "Hello, \(name.capitalized)!"
+        return UserResponse(systemMessage: "CandleHub is coming soon!",
+                            contentMessage: message,
+                            contactMessage: "by @voity_vit")
+    }
+    
+    app.post("user-info") { req async throws -> UserResponse in
+        let userInfo = try req.content.decode(UserInfo.self)
+        let message = "Hello, \(userInfo.name.capitalized)! You are \(userInfo.age) years old."
+        return UserResponse(systemMessage: "CandleHub is coming soon!",
+                            contentMessage: message,
+                            contactMessage: "by @voity_vit")
+    }
+    
     try app.register(collection: TodoController())
 }
+
+struct UserResponse: Content {
+    let systemMessage: String
+    let contentMessage: String
+    let contactMessage: String
+}
+
+struct UserInfo: Content {
+    let name: String
+    let age: Int
+}
+
+
